4a2789b199059c3655be70e091f8e1ffb5dbffd9
10434542 <ruben.timmermans@live.nl>
2023-12-28 21:51:25 +0100

    start part two day 4

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -2,10 +2,14 @@ import kotlin.math.pow
 
 fun main() {
     var resultPartOne = 0
-    "dayFour.txt".asResource()
+    val allCards = "dayFour.txt".asResource()
         .lines()
         .filter { it.isNotEmpty() }
-        .forEach { line ->
+    val amountOfCards = allCards.map { 1 }.toMutableList()
+    // below is correct
+//    val amountOfCards = mutableListOf(1,2,3,4,5,6)
+    allCards
+        .forEachIndexed { index, line ->
             val cardAndHands = line.split(":")
             val cards = cardAndHands[1].split("|")
             val firstCard = cards[0].trim().split(" ").filter { it.isNotEmpty() }
@@ -15,6 +19,21 @@ fun main() {
             val amountOfMatches = firstSet.intersect(secondSet).size.toDouble()
             val tempResult = 2.0.pow(amountOfMatches - 1)
             resultPartOne += tempResult.toInt()
+            print("${amountOfMatches.toInt()} ")
+            for (currentCard in 1..amountOfCards[index]) {
+                print("$currentCard found extra cards for ")
+                for(i in 1..amountOfMatches.toInt()) {
+                    print("${index + i + 1}")
+                    // increase values of amountOfCards here, take into account the size of amountOfCards
+                }
+
+
+            }
+            println("")
         }
+    print(amountOfCards)
     println("result for day four part one is $resultPartOne")
+
+
+
 }
