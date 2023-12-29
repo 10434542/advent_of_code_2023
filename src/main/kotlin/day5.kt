2c358f733cd6c1d70ec00ec72bf02bc6df8670c1
ruben <ruben.timmermans@live.nl>
2023-12-29 22:02:49 +0100

    solution part one day 5

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -1,16 +1,22 @@
 fun main() {
     val bla = "dayFive.txt".asResource().split("\n").mapNotNull { line -> line.trimIndent().takeIf { it.isNotEmpty() } }
-    println("result is  ${test(bla)}")
+    val seedList = bla[0].split(":")[1].trim().split(" ").map{ it.toLong()}
+    val result = seedList.map { seed ->
+        test(bla, seed)
+    }
+    result.forEach { println(it) }
+
 }
 
-fun test(input: List<String>): Long = with(input.iterator()) {
-    var value = next().removePrefix("seeds:").split(" ").mapNotNull { it.toLongOrNull() }.min()
+fun test(input: List<String>, whatever: Long): Long = with(input.iterator()) {
+    var value = whatever
     for (line in this) {
-        if (line.isBlank() || line.first().isLetter()) continue
-        val (dstStart, srcStart, length) = line.split(" ").map { it.toLong() }
-        if (value in srcStart..<srcStart + length) {
-            println("value in range is $value")
-            value = dstStart + value - srcStart
+        println(line)
+        if (line.first().isLetter()) continue
+        val (destinationStart, sourceStart, length) = line.split(" ").map { it.toLong() }
+        if (value in sourceStart..<sourceStart + length) {
+            value = destinationStart + value - sourceStart
+            // find next part of mapping
             while (hasNext()) {
                 val nextLine = next()
                 if (nextLine.isNotBlank() && nextLine.first().isLetter()) break
